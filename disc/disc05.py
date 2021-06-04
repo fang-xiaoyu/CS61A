9a9d23a69266fd96f5b0401ab0c1ce342e6502ae
fang-xiaoyu <78073286+fang-xiaoyu@users.noreply.github.com>
2021-06-04 20:06:56 +0800

    Create disc05.py

---
+++ disc/disc05.py
@@ -0,0 +1,127 @@
+#%%
+# Tree ADT
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
+#%%
+def even_weighted(lst):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted(x)
+    [0, 6, 20]
+    """
+    return [i * lst[i] for i in range(len(lst)) if i % 2 == 0]
+from doctest import run_docstring_examples
+run_docstring_examples(even_weighted, globals(), True)
+# %%
+def max_product(lst):
+    """Return the maximum product that can be formed using lst
+    without using any consecutive numbers
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    """
+    if any(lst):
+        value_lst = []
+        for i in range(len(lst)):
+            if i == 0:
+                new_lst = lst[2:]
+            elif i == len(lst) - 1:
+                new_lst = lst[:-2]
+            else:
+                new_lst = lst[:i-1] + lst[i+2:]
+            value_lst += [lst[i] * max_product(new_lst)]
+        return max(value_lst)
+    else:
+        return 1
+    """
+    if lst == []:
+        return 1
+    else:
+        return max(max_product(lst[1:]), lst[0] * max_product(lst[2:]))
+run_docstring_examples(max_product, globals(), True)
+# %%
+def redundant_map(t, f):
+    """
+    >>> double = lambda x: x*2
+    >>> t = tree(1, [tree(1), tree(2, [tree(1, [tree(1)])])])
+    >>> print_tree(t)
+    1
+      1
+      2
+        1
+          1
+    >>> new_t = redundant_map(t, double)
+    >>> print_tree(new_t)
+    2
+      4
+      8
+        16
+          256
+    """
+    new_label = f(label(t))
+    new_f = lambda x: f(f(x))
+    new_b = [redundant_map(b, new_f) for b in branches(t)]
+    return tree(new_label, new_b)
+run_docstring_examples(redundant_map, globals(), True)
