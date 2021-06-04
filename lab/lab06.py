12347f90b584fa347d3a2ecb0567d43509775b7f
fang-xiaoyu <78073286+fang-xiaoyu@users.noreply.github.com>
2021-06-04 20:15:47 +0800

    Create lab06.py

---
+++ lab/lab06.py
@@ -0,0 +1,37 @@
+#%%
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    def adder(m):
+        nonlocal n
+        n += 1
+        return m + n - 1
+    return adder
+from doctest import run_docstring_examples
+run_docstring_examples(make_adder_inc, globals(), True)
+#%%
+def map(fn, lst):
+    """Maps fn onto lst using mutation.
+    >>> original_list = [5, -1, 2, 0]
+    >>> map(lambda x: x * x, original_list)
+    >>> original_list
+    [25, 1, 4, 0]
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(len(lst)):
+        lst[i] = fn(lst[i])
+run_docstring_examples(map, globals(), True)
+# %%
