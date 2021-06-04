c62542d8a53a41c7f0d672666c96ff5ef07b6ee8
fang-xiaoyu <78073286+fang-xiaoyu@users.noreply.github.com>
2021-06-04 20:14:43 +0800

    Create lab04.py

---
+++ lab/lab04.py
@@ -0,0 +1,232 @@
+""" Lab 04: Lists and Data Abstraction """
+#%%
+def couple(s1, s2):
+    """Return a list that contains lists with i-th elements of two sequences
+    coupled together.
+    >>> s1 = [1, 2, 3]
+    >>> s2 = [4, 5, 6]
+    >>> couple(s1, s2)
+    [[1, 4], [2, 5], [3, 6]]
+    >>> s3 = ['c', 6]
+    >>> s4 = ['s', '1']
+    >>> couple(s3, s4)
+    [['c', 's'], [6, '1']]
+    """
+    assert len(s1) == len(s2)
+    "*** YOUR CODE HERE ***"
+    new = []
+    for i in range(len(s1)):
+        new.append([s1[i], s2[i]])
+    return new
+
+from doctest import run_docstring_examples
+run_docstring_examples(couple, globals(), True)
+#%%
+def enumerate(s, start=0):
+    """Returns a list of lists, where the i-th list contains i+start and
+    the i-th element of s.
+    >>> enumerate([6, 1, 'a'])
+    [[0, 6], [1, 1], [2, 'a']]
+    >>> enumerate('five', 5)
+    [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
+    """
+    "*** YOUR CODE HERE ***"
+    new = []
+    for i in range(len(s)):
+        new.append([i+start, s[i]])
+    return new 
+run_docstring_examples(enumerate, globals(), True)
+#%%
+def make_city(name, lat, lon):
+    """
+    >>> berkeley = make_city('Berkeley', 122, 37)
+    >>> new_york = make_city('New York City', 74, 40)
+    """
+    city = [name, lat, lon]
+    return city
+
+def get_name(city):
+    """
+    >>> get_name(berkeley)
+    'Berkeley'
+    """
+    return city[0]
+
+def get_lat(city):
+    """
+    >>> get_lat(berkeley)
+    122
+    """
+    return city[1]
+
+def get_lon(city):
+    """
+    >>> get_lon(berkeley)
+    40
+    """
+    return city[2]
+berkeley = make_city('Berkeley', 122, 37)
+print(get_name(berkeley))
+print(get_lat(berkeley))
+print(get_lon(berkeley))
+#%%
+from math import sqrt
+def distance(city1, city2):
+    """
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    """
+    "*** YOUR CODE HERE ***"
+    lat1, lon1 = get_lat(city1), get_lon(city1)
+    lat2, lon2 = get_lat(city2), get_lon(city2)
+    return sqrt((lat1 - lat2)**2 + (lon1 - lon2)**2)
+run_docstring_examples(distance, globals(), True)
+#%%
+def closer_city(lat, lon, city1, city2):
+    """
+    Returns the name of either city1 or city2, whichever is closest to
+    coordinate (lat, lon).
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    "*** YOUR CODE HERE ***"
+    default = make_city('default', lat, lon)
+    distance_city1 = distance(city1, default)
+    distance_city2 = distance(city2, default)
+    if distance_city1 > distance_city2:
+        return get_name(city2)
+    else: 
+        return get_name(city1)
+run_docstring_examples(closer_city, globals(), True)
+#%%
+def check_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    >>> change_abstraction(False)
+    """
+
+
+# Treat all the following code as being behind an abstraction layer, you shouldn't need to look at it!
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name" : name, "lat" : lat, "lon" : lon}
+    else:
+        return [name, lat, lon]
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+change_abstraction.changed = False
+
+
+""" Lab 04 Optional Questions """
+#%%
+from math import sqrt
+from lab04 import *
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    new = []
+    for element in s:
+        sqrt_ele = sqrt(element)
+        if round(sqrt_ele) ** 2 == element:
+            new.append(int(sqrt_ele))
+    return new
+from doctest import run_docstring_examples
+run_docstring_examples(squares, globals(), True)
+#%%
+def key_of_min_value(d):
+    """Returns the key in a dict d that corresponds to the minimum value of d.
+    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
+    >>> min(letters)
+    'a'
+    >>> key_of_min_value(letters)
+    'c'
+    """
+    "*** YOUR CODE HERE ***"
+    return min(d, key = d.get)
+run_docstring_examples(key_of_min_value, globals(), True)
+# %%
